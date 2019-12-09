17110559af638417da1a0207eec777c512db2871
Anton Simonov <simonovay@pochtabank.ru>
2019-12-09 23:00:00 +0300

    cache producerTemplate

--- src/main/kotlin/com/github/simoant/drools/analyzer/camel/CamelDataRequestProcessor.kt
+++ src/main/kotlin/com/github/simoant/drools/analyzer/camel/CamelDataRequestProcessor.kt
@@ -4,19 +4,16 @@ import com.github.simoant.drools.analyzer.IDataRequestProcessor
 import com.github.simoant.drools.analyzer.X_UUID_NAME
 import com.github.simoant.drools.analyzer.model.DataRequest
 import com.github.simoant.drools.analyzer.utils.log
-import org.apache.camel.CamelContext
 import org.apache.camel.ProducerTemplate
 import java.util.concurrent.CompletableFuture
 
-data class CamelDataRequestProcessor (val camelContext: CamelContext) : IDataRequestProcessor {
-    val producerTemplate: ProducerTemplate = camelContext.createProducerTemplate()
-
+data class CamelDataRequestProcessor(val producerTemplate: ProducerTemplate) : IDataRequestProcessor {
     override fun executeAsync(request: DataRequest, trackId: String): CompletableFuture<Any?> {
         val futureRes = producerTemplate.asyncRequestBodyAndHeader(request.uri, request.requestData,
             X_UUID_NAME, trackId)
         return futureRes
-            .exceptionally {
-                log.debug("CamelDataRequestProcessor: Null response for request: $request")
+            .exceptionally { error ->
+                log.error("CamelDataRequestProcessor: Error response for request: $request, ex: $error")
                 null
             }
             .thenApply { it }
