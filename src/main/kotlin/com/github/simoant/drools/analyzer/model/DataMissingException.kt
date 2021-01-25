9f65d435ecb7dd903860dcc0d4d2abb7fca4fa07
Anton Simonov <simonovay@pochtabank.ru>
2021-01-25 18:39:08 +0300

    fix DataMissingException

--- src/main/kotlin/com/github/simoant/drools/analyzer/model/DataMissingException.kt
+++ src/main/kotlin/com/github/simoant/drools/analyzer/model/DataMissingException.kt
@@ -1,7 +1,5 @@
 package com.github.simoant.drools.analyzer.model
 
-import java.lang.RuntimeException
-
-class DataMissingException(val msg: String) : RuntimeException() {
+class DataMissingException(val msg: String) : RuntimeException(msg) {
 
 }
